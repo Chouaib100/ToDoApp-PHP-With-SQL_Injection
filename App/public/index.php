060d514ccb4a887e0f190676a1eaf376697d8be8
Chouaib <chouaibzouine1contact@gmail.com>
2025-01-20 17:40:55 +0100

    le fichier index.php qui servira de point d'entrée à votre application.

---
+++ App/public/index.php
@@ -0,0 +1,11 @@
+
+<?php
+// Démarrer la session
+session_start();
+
+// Inclure la configuration de la base de données
+require __DIR__ . '/../config/database.php';
+
+// Inclure le fichier de routes
+require __DIR__ . '/../config/routes.php';
+?>
